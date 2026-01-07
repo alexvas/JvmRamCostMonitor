3b145d566112467c8a15aea7c4aacd8af04ab53b
Alexander Vasilev <a.a.vasiljev+jvmramcostmonitor@yandex.ru>
2026-01-07 14:11:05 +0700

    Генерирую Rust-клиента grpc по protobuf-описанию

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,13 @@
-fn main() {
-    tauri_build::build()
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_prost_build::configure()
+        .build_server(false)
+        .out_dir("src/proto") // you can change the generated code's location
+        .compile_protos(
+            &["../proto/protocol.proto"],
+            &["../proto"], // specify the root location to search proto dependencies
+        )?;
+
+    tauri_build::build();
+
+    Ok(())
 }
